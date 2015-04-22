ace3b910ea8ac242c2f433d6fe9aa6df2d035827
marthaurion <marthaurion@gmail.com>
2015-04-21 19:58:44 -0500

    Created method stubs in Admin Controller.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,6 +7,36 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller {
 
-	//
+    /**
+     * Main page for admin page
+     */
+	public function index()
+    {
+        //
+    }
+
+    /**
+     * Category creation page
+     */
+    public function categories()
+    {
+        //
+    }
+
+    /**
+     * Log viewing page
+     */
+    public function logs()
+    {
+        //
+    }
+
+    /**
+     * Comment moderation page
+     */
+    public function comments()
+    {
+        //
+    }
 
 }
--- readme.md
+++ readme.md
@@ -4,7 +4,6 @@ Building a website using Laravel just to learn stuff. Hopefully this works.
 
 Notes to self:
 - Nested comments need to work.
-- E-mailing from the server needs to work.
 - Need to support creating tags from post creation.
 - Need an admin site for personal use and management. Stuff to include:
 	- creation of categories
