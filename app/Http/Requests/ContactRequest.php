c16468eece6901caaf655d730ee932d03cbae88b
marthaurion <marthaurion@gmail.com>
2015-04-21 19:42:47 -0500

    Update class name for ContactRequest.

--- app/Http/Requests/ContactRequest.php
+++ app/Http/Requests/ContactRequest.php
@@ -2,7 +2,7 @@
 
 use App\Http\Requests\Request;
 
-class CommentRequest extends Request {
+class ContactRequest extends Request {
 
     /**
      * Determine if the user is authorized to make this request.
