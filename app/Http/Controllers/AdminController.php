9c870c018afb391e12990417cbf4d0fd18caa0a7
marthaurion <marthaurion@gmail.com>
2015-04-22 11:44:14 -0500

    Restrict admin page to super users.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller {
 
+    public function __construct()
+    {
+        $this->middleware('super');
+    }
+
     /**
      * Main page for admin page
      */
